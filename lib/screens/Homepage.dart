a4772b3287e660218b903cd7c25dbb1d5086a423
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2023-12-22 13:43:29 +0530

    added basic home page welcome & avatar

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:veranco/utils/colors.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -11,6 +11,37 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Welcome', style: TextStyle(color: darkGrayColor)),
+                        Text('Name of the user'),
+                      ],
+                    ),
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://img.freepik.com/free-icon/user_318-563642.jpg?w=360'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
